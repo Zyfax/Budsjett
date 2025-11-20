30f24cb26c5e5d5e648465ffe22fd6bb2b78dd65
olsenfoto1 <105080666+olsenfoto1@users.noreply.github.com>
2025-11-20 17:42:16 +0100

    Revert "Add transaction search suggestions and detail view"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,6 @@ import CategoriesPage from './pages/CategoriesPage.jsx';
 import PagesPage from './pages/PagesPage.jsx';
 import SettingsPage from './pages/SettingsPage.jsx';
 import FixedExpensesPage from './pages/FixedExpensesPage.jsx';
-import TransactionDetailPage from './pages/TransactionDetailPage.jsx';
-import TransactionSearch from './components/TransactionSearch.jsx';
 
 const getInitialTheme = () => {
   if (typeof window === 'undefined') return 'light';
@@ -167,7 +165,6 @@ const App = () => {
               📲 Installer appen
             </button>
           )}
-          <TransactionSearch />
           <div className="nav-links">
             <NavLink to="/" end>
               Oversikt
@@ -190,7 +187,6 @@ const App = () => {
           <Route path="/categories" element={<CategoriesPage />} />
           <Route path="/pages" element={<PagesPage />} />
           <Route path="/innstillinger" element={<SettingsPage />} />
-          <Route path="/transaksjon/:id" element={<TransactionDetailPage />} />
         </Routes>
       </main>
     </div>
